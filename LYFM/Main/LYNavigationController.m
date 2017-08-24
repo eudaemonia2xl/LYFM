8c8eeef62eba8c3c5bdc7229ff70bfa98e7832af
zhengxueli <337388427@qq.com>
2017-08-24 18:07:54 +0800

    自定义导航控制器

--- LYFM/Main/LYNavigationController.m
+++ LYFM/Main/LYNavigationController.m
@@ -14,9 +14,27 @@
 
 @implementation LYNavigationController
 
++ (void)initialize
+{
+    UINavigationBar *navBar = [UINavigationBar appearance];
+    [navBar setBackgroundImage:[UIImage imageNamed:@"navigationbar_bg_64"] forBarMetrics:UIBarMetricsDefault];
+}
+    
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+}
+    
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+    [super pushViewController:viewController animated:animated];
+    
+    self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"btn_back_n"] style:UIBarButtonItemStylePlain target:self action:@selector(back)];
+}
+    
+- (void)back
+{
+    [self popViewControllerAnimated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
